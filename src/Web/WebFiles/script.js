6d6cb4b7b5b16749b15081c3147239601a53e408
NicholasTurcas <turcasnicholas10@gmail.com>
2026-08-30 12:02:36 +0300

    Add sensor fictive data to dashboard

---
+++ src/Web/WebFiles/script.js
@@ -0,0 +1,30 @@
+console.log("NovaNest JavaScript pornit!");
+
+const temperatureElement = document.getElementById("temperature");
+console.log(temperatureElement);
+const temperature = 27.5; 
+temperatureElement.textContent = temperature + " °C";
+
+const humidityElement = document.getElementById("humidity");
+const humidity = 60;
+
+humidityElement.textContent = humidity + " %";
+
+const powerElement = document.getElementById("powerConsumption");
+const power = 125;
+
+powerElement.textContent = power + " W";
+
+const solarElement = document.getElementById("solarProduction");
+const solar = 85;
+
+solarElement.textContent = solar + " W";
+
+const batteryElement = document.getElementById("batteryLevel");
+const battery = 78; 
+
+batteryElement.textContent = battery + " %";
+
+const systemElement = document.getElementById("systemOnline");
+const systemOnline = true;
+systemElement.textContent = systemOnline ? "Online" : "Offline";    
